1aee9dda55a3791085cc3cdca84387dc66670a0b
Sothyra Chan <schan256@my.centennialcollege.ca>
2026-02-22 06:00:43 +0000

    worked and passed the first 4 unit tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,5 +5,32 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function() {
-    convertHandler;
+    suite('convertHandler.getNum(input)', function() {
+      test('Test Whole Number Input', function() {
+        const inputVal = "10l";
+        assert.equal(convertHandler.getNum(inputVal), 10, 'Value should be a whole number!');
+      });
+
+      test('Test Decimal Number Input', function() {
+        const inputVal = "5.5mi";
+        assert.equal(convertHandler.getNum(inputVal), 5.5, 'Value should be a decimal number!');
+      });
+
+      test('Test Fractional Number Input', function() {
+        const inputVal = "5/3km";
+        assert.equal(convertHandler.getNum(inputVal), 5/3, 'Value should be a fractional number!');
+      });
+
+      test('Test Fractional Number with Decimal Input', function() {
+        const inputVal = "2.3/2kg";
+        assert.equal(convertHandler.getNum(inputVal), 2.3/2, 'Value should be a fractional input with decimal number!');
+      });
+
+      test('Test An Error on Double-Fraction Input', function() {
+        const actualInputVal = "2/5/8lbs";
+        const expectedInputVal = "2/5lbs";
+        assert.fail(convertHandler.getNum(actualInputVal), expectedInputVal, 'Value like double-fraction should not be allowed', '2/5');
+      });
+
+    });
 });
